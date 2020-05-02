017e64381eea7df53b4312d530620b7d432b86a5
MarcBackwinkel <marc.backwinkel@gmail.com>
2020-05-02 22:20:29 +0000

    update

--- Chapter1/dist/todoItem.js
+++ Chapter1/dist/todoItem.js
@@ -1,20 +1,18 @@
-export class TodoItem {
-    public id: number;
-    public task: string;
-    public complete: boolean = false;
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class TodoItem {
     //public is used in constructor to make clear concise constructor syntax is being used
-    public constructor(id: number, task: string, complete: boolean = false){
-        
+    constructor(id, task, complete = false) {
+        this.complete = false;
         //no statements required, following statements are done automatically
         //this.id = id;
         //this.task = task;
         //this.complete = complete;
     }
-
     //public is standard if not given differently
-    printDetails(): void {
+    printDetails() {
         //  \t --> Tab
-        console.log(`${this.id}\t${this.task} ${this.complete ? "\t(complete)": ""}`);
+        console.log(`${this.id}\t${this.task} ${this.complete ? "\t(complete)" : ""}`);
     }
 }
+exports.TodoItem = TodoItem;
