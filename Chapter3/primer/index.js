c6b90cf8d5b9a9a93865fd0b3dd83487438773f4
MarcBackwinkel <marc.backwinkel@gmail.com>
2020-05-17 12:58:04 +0000

    avoiding argument mismatch problems

--- Chapter3/primer/index.js
+++ Chapter3/primer/index.js
@@ -3,20 +3,16 @@ console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
 
-if (hatPrice === bootsPrice){
-    console.log("Prices are the same");
-} else {
-    console.log("Prices are different");
+function sumPrices(...numbers){
+    return numbers.reduce((total, val) => 
+        total + (Number.isNaN(Number(val)) ? 0 : Number(val)));
 }
 
-let totalPrice = Number(hatPrice) + Number(bootsPrice);
-console.log(`Total price: ${totalPrice}`);
+let totalPrice = sumPrices(hatPrice, bootsPrice);
+console.log(`Total: ${totalPrice} ${typeof(totalPrice)}`);
 
-let myVariable = "Adam";
-console.log(`Type: ${typeof(myVariable)}`);
-myVariable = 100;
-console.log(`Type: ${typeof(myVariable)}`);
+totalPrice = sumPrices(100, 200, 300);
+console.log(`Total: ${totalPrice} ${typeof(totalPrice)}`);
 
-let firstCity = "Amsterdam";
-let secondCity = firstCity || "London";
-console.log(`City: ${secondCity}`);
+totalPrice = sumPrices(100, 200);
+console.log(`Total: ${totalPrice} ${typeof(totalPrice)}`);
