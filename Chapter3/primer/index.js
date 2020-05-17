86b90b32803adbaa0af2c414e6f921fb730bbd7e
MarcBackwinkel <marc.backwinkel@gmail.com>
2020-05-17 13:48:42 +0000

    reduce function understood

--- Chapter3/primer/index.js
+++ Chapter3/primer/index.js
@@ -4,15 +4,21 @@ let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
 
 function sumPrices(...numbers){
-    return numbers.reduce((total, val) => 
-        total + (Number.isNaN(Number(val)) ? 0 : Number(val)));
+    return numbers.reduce((total, val) => {
+        let retItem = total + (Number.isNaN(Number(val)) ? 0 : Number(val));
+        console.log(`-- total: ${total} , val: ${val} = ${retItem}`);
+        return retItem;
+    },0);
 }
 
+console.log("1. Example");
 let totalPrice = sumPrices(hatPrice, bootsPrice);
 console.log(`Total: ${totalPrice} ${typeof(totalPrice)}`);
 
+console.log("2. Example");
 totalPrice = sumPrices(100, 200, 300);
 console.log(`Total: ${totalPrice} ${typeof(totalPrice)}`);
 
+console.log("3. Example");
 totalPrice = sumPrices(100, 200);
 console.log(`Total: ${totalPrice} ${typeof(totalPrice)}`);
