d55c860ed05c2b61157922f16632321babc1fd76
MarcBackwinkel <marc.backwinkel@gmail.com>
2020-06-12 11:54:37 +0000

    Asserting to an unexpected type

---
+++ Chapter7/types/src/index.ts
@@ -0,0 +1,74 @@
+let myVar;
+
+myVar = 12;
+console.log(`${myVar} = ${typeof(myVar)}`);
+myVar = "Hello";
+console.log(`${myVar} = ${typeof myVar}`);
+myVar = true;
+console.log(`${myVar} = ${typeof myVar}`);
+
+function calculateTax(amount){
+    return amount * 1.2;
+}
+console.log(`${12} = ${calculateTax(12)}`);
+console.log(`${"Hello"} = ${calculateTax("Hello")}`);
+console.log(`${true} = ${calculateTax(true)}`);
+
+//with Type Annotations
+function calculateTaxWithTypeAnno(amount: number): number{
+    return amount * 1.2;
+}
+console.log(`${12} = ${calculateTaxWithTypeAnno(12)}`);
+//console.log(`${"Hello"} = ${calculateTaxWithTypeAnno("Hello")}`);
+//console.log(`${true} = ${calculateTaxWithTypeAnno(true)}`);
+let price: number = 100;
+let taxAmount: number = calculateTaxWithTypeAnno(price);
+let halfShare: number = taxAmount / 2;
+console.log(`Full amount in tax: ${taxAmount}`);
+console.log(`Half share: ${halfShare}`);
+
+//Implicitly Defined Static Types
+function calculateTaxImplicitly(amount: number){
+    //return amount * 1.2;  --> function returns NUMBER as input parameter is a number
+    return (amount * 1.2).toFixed(2);   //--> function returns a string!
+}
+let priceImpl = 100;
+let taxAmountImpl = calculateTaxImplicitly(priceImpl);
+//let halfShareImpl = taxAmountImpl / 2;
+console.log(`Full amount in tax: ${taxAmountImpl}`);
+//console.log(`Half share: ${halfShareImpl}`);
+
+//Using the any type
+function calculateTaxWithAny(amount: any): any {
+    return `$${(amount * 1.2).toFixed(2)}`;
+}
+let priceAny = 100;
+let taxAmountAny = calculateTaxWithAny(priceAny);
+let halfShareAny = taxAmountAny / 2;
+console.log(`Price: ${priceAny}`);
+console.log(`Full amount in tax: ${taxAmountAny}`);
+console.log(`Half share: ${halfShareAny}`);
+let newResult: any = calculateTaxWithAny(200);
+let myNumber: number = newResult;
+//console.log(`Number value: ${myNumber.toFixed(2)}`);
+
+//Using Implicitly Defined Any Types
+function calculateTaxImplAny(amount): any {
+    return `$${(amount * 1.2).toFixed(2)}`;
+}
+let personalVal = calculateTaxImplAny("Bob");
+console.log(`Name: ${personalVal}`);
+
+//Using Type Unions
+function calculateTaxUnion(amount: number, format: boolean): number | string {
+    const calcAmount = amount * 1.2;
+    return format ? `$${calcAmount.toFixed(2)}` : calcAmount;
+}
+let taxNumber = calculateTaxUnion(100, false) as number;
+let taxString = calculateTaxUnion(100, true) as string;
+let taxBoolean = calculateTaxUnion(100, false) as any as boolean;
+console.log(`Number Value: ${taxNumber.toFixed(2)}`);
+console.log(`String Value: ${taxString.charAt(0)}`);
+console.log(`Boolean Value: ${taxBoolean}`);
+
+//Using a type guard
